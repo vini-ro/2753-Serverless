70a9e3777a19dd8993f04f1fe1fc732d86e9d2f3
Vinicius <vroliveira.eng@gmail.com>
2025-01-13 10:03:06 -0300

    Autenticação basic

--- api/handlers.js
+++ api/handlers.js
@@ -1,11 +1,54 @@
 'use strict';
 const { MongoClient, ObjectId } = require('mongodb');
+const { pbkd2Sync } = require('crypto');
 
 async function connectToDatabase() {
     const client = new MongoClient(process.env.MONGODB_CONNECTIONSTRING)
     const connection = await client.connect()
     return connection.db(process.env.MONGODB_DB_NAME)
 }
+
+async function basicAuth(event) {
+  const{authorization} = event.headers;
+  if (!authorization) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Missing authorization header' }),
+    }
+  }
+
+  const [type, credentials] = authorization.split(' ');
+  if (type !== 'Basic') {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Unsupported authorization type' }),
+    }
+  }
+
+  const [username, password] = Buffer.from(credentials, 'base64').toString('utf-8').split(':');
+  const hashedPass = pbkd2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex');
+
+  const client = await connectToDatabase()
+  const collection = await client.collection('users')
+  const user = await collection.findOne({
+    name: username,
+    password: hashedPass
+  })
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Invalid credentials' }),
+    }
+  }
+
+  return {
+    id: user._id,
+    username: user.username
+  }
+}
+
+
 
 function extractBody(event) {
   if (!event?.body) {
@@ -18,6 +61,9 @@ function extractBody(event) {
 }
 
 module.exports.sendResponse = async (event) => {
+  const authResult = await basicAuth(event);
+  if (authResult.statusCode == 401) return authResult;
+
   const { name, answers } = extractBody(event);
   const correctQuestions = [3, 1, 0, 2];
   const totalCorrectAnswers = answers.reduce((acc, answer, index) => {
@@ -54,6 +100,8 @@ return {
 }
 
 module.exports.getResult = async (event) => {
+  if (authResult.statusCode == 401) return authResult;
+
   const client = await connectToDatabase()
   const collection = await client.collection('results')
 
